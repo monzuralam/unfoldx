d550751c8ec60d28856387b8fccd1ec34fc96810
monzuralam <monzur707@gmail.com>
2021-11-22 21:06:11 +0600

    post_class & id added

--- index.php
+++ index.php
@@ -313,7 +313,7 @@ get_header();
                         $blog->the_post();
                         ?>
                         <div class="col-lg-4 col-md-8 col-sm-9">
-                            <div class="single-blog mt-30">
+                            <div id="post-<?php the_ID(); ?>" <?php post_class('single-blog mt-30'); ?>>
                                 <div class="blog-image">
                                     <?php
                                         if( has_post_thumbnail() ){
